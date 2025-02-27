4274f187f44c93aaba7c513ca975d349a5d06306
WictorBertulino <wictorbertulino@gmail.com>
2025-02-27 07:39:15 -0400

    corrigindo rota

--- src/routes/autorRoutes.js
+++ src/routes/autorRoutes.js
@@ -8,6 +8,6 @@ router.get('/autores', AutorController.listarAutores);
 router.get('/autores/:id', AutorController.listarAutor);
 router.put('/autores/:id', AutorController.atualizarAutor);
 router.post('/autores', AutorController.cadastrarAutor);
-router.delete('/livro/:id', AutorController.deletarAutor);
+router.delete('/autores/:id', AutorController.deletarAutor);
 
 export default router; // Exporta o router para ser usado em outros arquivos
